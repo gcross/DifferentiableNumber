0a991acd47c91cdb7af4bd572cb2d82489eada43
Gregory Crosswhite <gcross@phys.washington.edu>
2009-12-12 15:49:19 -0800

    Removed |+| and |-|, and used the Num class instead.

--- sources/Data/Differentiable/FunctionExpansion.hs
+++ sources/Data/Differentiable/FunctionExpansion.hs
@@ -33,38 +33,39 @@ type DifferentialOperatorTransformation a = DifferentialOperator a -> Differenti
 -- @-node:gcross.20091208183517.1445:DifferentialOperatorTransformation
 -- @-node:gcross.20091208183517.1407:Types
 -- @+node:gcross.20091208183517.1429:Instances
+-- @+node:gcross.20091212141130.1582:FunctionExpansion
 -- @+node:gcross.20091208183517.1430:Differentiable
 instance Differentiable (FunctionExpansion a) where
     differentiateBy index (argument :-> expansion) = argument :-> d index expansion
 -- @-node:gcross.20091208183517.1430:Differentiable
--- @+node:gcross.20091208183517.1440:Show (FunctionExpansion a)
+-- @+node:gcross.20091208183517.1440:Show
 instance Show a => Show (FunctionExpansion a) where
     show (argument :-> value) = show argument ++ " :-> " ++ show value
--- @-node:gcross.20091208183517.1440:Show (FunctionExpansion a)
--- @+node:gcross.20091208183517.1449:Eq (FunctionExpansion a)
+-- @-node:gcross.20091208183517.1440:Show
+-- @+node:gcross.20091208183517.1449:Eq
 instance Eq a => Eq (FunctionExpansion a) where
     (_ :-> value1) == (_ :-> value2) = value1 == value2
--- @-node:gcross.20091208183517.1449:Eq (FunctionExpansion a)
--- @+node:gcross.20091212141130.1444:Ord (FunctionExpansion a)
+-- @-node:gcross.20091208183517.1449:Eq
+-- @+node:gcross.20091212141130.1444:Ord
 instance Ord a => Ord (FunctionExpansion a) where
     compare (_ :-> value1) (_ :-> value2) = compare value1 value2
--- @-node:gcross.20091212141130.1444:Ord (FunctionExpansion a)
--- @+node:gcross.20091208183517.1436:Num (FunctionExpansion a)
+-- @-node:gcross.20091212141130.1444:Ord
+-- @+node:gcross.20091208183517.1436:Num
 instance Num a => Num (FunctionExpansion a) where
     (+) = binaryFunctionExpansionOperator (+)
     (-) = binaryFunctionExpansionOperator (-)
     (*) = binaryFunctionExpansionOperator (*)
     negate = unaryFunctionExpansionOperator negate
     fromInteger value = [] :-> (constant . fromInteger $ value)
--- @-node:gcross.20091208183517.1436:Num (FunctionExpansion a)
--- @+node:gcross.20091212141130.1438:Fractional (FunctionExpansion a)
+-- @-node:gcross.20091208183517.1436:Num
+-- @+node:gcross.20091212141130.1438:Fractional
 instance Fractional a => Fractional (FunctionExpansion a) where
     (/) = binaryFunctionExpansionOperator (/)
     recip = unaryFunctionExpansionOperator recip
     fromRational value = [] :-> (constant . fromRational $ value)
 
--- @-node:gcross.20091212141130.1438:Fractional (FunctionExpansion a)
--- @+node:gcross.20091212141130.1440:Floating (FunctionExpansion a)
+-- @-node:gcross.20091212141130.1438:Fractional
+-- @+node:gcross.20091212141130.1440:Floating
 instance Floating a => Floating (FunctionExpansion a) where
     pi = [] :-> constant pi
     exp = unaryFunctionExpansionOperator exp
@@ -83,9 +84,54 @@ instance Floating a => Floating (FunctionExpansion a) where
     asinh = unaryFunctionExpansionOperator asinh
     atanh = unaryFunctionExpansionOperator atanh
     acosh = unaryFunctionExpansionOperator acosh
--- @-node:gcross.20091212141130.1440:Floating (FunctionExpansion a)
+-- @-node:gcross.20091212141130.1440:Floating
+-- @-node:gcross.20091212141130.1582:FunctionExpansion
+-- @+node:gcross.20091212141130.1583:Function
+-- @+node:gcross.20091212141130.1585:Show
+instance Show b => Show (a -> b) where
+    show = undefined
+-- @-node:gcross.20091212141130.1585:Show
+-- @+node:gcross.20091212141130.1587:Eq
+instance Eq b => Eq (a -> b) where
+    (==) = undefined
+-- @-node:gcross.20091212141130.1587:Eq
+-- @+node:gcross.20091212141130.1589:Num
+instance Num b => Num (a -> b) where
+    (+) = liftA2 (+)
+    (-) = liftA2 (-)
+    (*) = liftA2 (*)
+    negate = (.) negate
+    fromInteger = const . fromInteger
+-- @-node:gcross.20091212141130.1589:Num
+-- @+node:gcross.20091212141130.1591:Fractional
+instance Fractional b => Fractional (a -> b) where
+    (/) = liftA2 (/)
+    recip = (.) recip
+    fromRational = const . fromRational
+-- @-node:gcross.20091212141130.1591:Fractional
+-- @+node:gcross.20091212141130.1593:Floating
+instance Floating b => Floating (a -> b) where
+    pi = const pi
+    exp = (.) exp
+    sqrt = (.) sqrt
+    log = (.) log
+    (**) = liftA2 (**)
+    logBase = liftA2 logBase
+    sin = (.) sin
+    cos = (.) cos
+    tan = (.) tan
+    asin = (.) asin
+    acos = (.) acos
+    atan = (.) atan
+    sinh = (.) sinh
+    cosh = (.) cosh
+    asinh = (.) asinh
+    atanh = (.) atanh
+    acosh = (.) acosh
+-- @-node:gcross.20091212141130.1593:Floating
+-- @-node:gcross.20091212141130.1583:Function
 -- @-node:gcross.20091208183517.1429:Instances
--- @+node:gcross.20091212141130.1433:Functions
+-- @+node:gcross.20091212141130.1433:Helpers
 -- @+node:gcross.20091212141130.1434:unaryFunctionExpansionOperator
 unaryFunctionExpansionOperator ::
     (DifferentiableNumber a -> DifferentiableNumber a) ->
@@ -101,7 +147,14 @@ binaryFunctionExpansionOperator op (argument1 :-> value1) (argument2 :-> value2)
     assert (argument1 == argument2) $
         argument1 :-> (value1 `op` value2)
 -- @-node:gcross.20091212141130.1436:binaryFunctionExpansionOperator
--- @-node:gcross.20091212141130.1433:Functions
+-- @-node:gcross.20091212141130.1433:Helpers
+-- @+node:gcross.20091212141130.1576:Functions
+-- @+node:gcross.20091212141130.1578:v_
+v_ :: (Enum i, Num a) => i -> [a] -> FunctionExpansion a
+v_ i argument = argument :-> ((argument !! index) ::> (replicate index zero ++ [one] ++ repeat zero))
+  where index = fromEnum i
+-- @-node:gcross.20091212141130.1578:v_
+-- @-node:gcross.20091212141130.1576:Functions
 -- @+node:gcross.20091208183517.1427:Differential Operators
 -- @+node:gcross.20091208183517.1431:multiplyByCoordinate
 multiplyByCoordinate :: (Num a, Enum i) => i -> DifferentialOperator a
@@ -120,7 +173,7 @@ infixl 7 *|
 -- @+node:gcross.20091208183517.1444:~~
 infixl 6 ~~
 (~~) :: Num a => DifferentialOperatorTransformation a
-a ~~ b = a.b |-| b.a
+a ~~ b = a.b - b.a
 -- @-node:gcross.20091208183517.1444:~~
 -- @+node:gcross.20091208183517.1456:|^
 infixl 8 |^
@@ -135,16 +188,10 @@ infixl 8 |^
   | otherwise
     = operator . operator |^(exponent-1)
 -- @-node:gcross.20091208183517.1456:|^
--- @+node:gcross.20091208183517.1452:|-|/|+|
-infixl 6 |+|, |-|
-(|+|), (|-|) :: Num a => DifferentialOperatorTransformation a
-(|+|) = liftA2 (+)
-(|-|) = liftA2 (-)
--- @-node:gcross.20091208183517.1452:|-|/|+|
 -- @+node:gcross.20091208183517.1454:osum/negosum
 osum, negosum :: Num a => [DifferentialOperator a] -> DifferentialOperator a
-osum = foldl1 (|+|)
-negosum = ((-1) *|) . osum
+osum = sum
+negosum = negate . osum
 -- @-node:gcross.20091208183517.1454:osum/negosum
 -- @-node:gcross.20091208183517.1442:Operators
 -- @-others
