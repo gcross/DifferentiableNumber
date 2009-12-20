89bd6b7fda7bb914db61373f0ca009f654215398
Gregory Crosswhite <gcross@phys.washington.edu>
2009-12-20 08:13:18 -0800

    Updated to use the new Blueprint conventions.

--- Setup.hs
+++ Setup.hs
@@ -12,22 +12,27 @@ module Main where
 
 -- @<< Import needed modules >>
 -- @+node:gcross.20091208183517.1474:<< Import needed modules >>
-import Blueprint.Tools.GHC.Simple
+import Blueprint.Tools.GHC.Main
 -- @-node:gcross.20091208183517.1474:<< Import needed modules >>
 -- @nl
 
 -- @+others
 -- @+node:gcross.20091208183517.1475:main
-main = defaultMain [("","sources")]
-                   (Just ([("","tests")],
-                    ["HUnit == 1.*"
-                    ,"QuickCheck == 2.*"
-                    ,"test-framework == 0.2.*"
-                    ,"test-framework-hunit == 0.2.*"
-                    ,"test-framework-quickcheck2 == 0.2.*"
-                    ,"random == 1.*"
-                    ]
-                   ))
+main = simpleDefaultMain
+    [("","sources")]
+    (Just
+        ([("","tests")]
+        ,[]
+        ,["HUnit == 1.*"
+         ,"QuickCheck == 2.*"
+         ,"test-framework == 0.2.*"
+         ,"test-framework-hunit == 0.2.*"
+         ,"test-framework-quickcheck2 == 0.2.*"
+         ,"random == 1.*"
+         ]
+        )
+    )
+    ["-O2","-fvia-C","-optc=-O3"]
 -- @-node:gcross.20091208183517.1475:main
 -- @-others
 -- @-node:gcross.20091208183517.1472:@thin Setup.hs
